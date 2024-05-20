ba1372d71183a2ccf182445eb6f0a27073f83993
aayushsingh9596 <aayushsingh9596@gmail.com>
2024-05-20 11:52:54 +0530

    t commit

--- src/routes/hotelRoutes.ts
+++ src/routes/hotelRoutes.ts
@@ -13,12 +13,13 @@ import {
   updateHotelById,
 } from "../controllers/hotelController";
 import uploadImages from "../cloudinary/uploadImagesToCloudinary";
+import path from "path";
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../images/hotelImages");
+    cb(null, path.resolve(__dirname, '../../images/hotelImages'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
