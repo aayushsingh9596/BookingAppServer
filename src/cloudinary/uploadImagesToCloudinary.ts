d2c80af2d4393288573823e263b056f8e3751ec5
aayushsingh9596 <aayushsingh9596@gmail.com>
2024-05-20 13:40:04 +0530

    t commit

--- src/cloudinary/uploadImagesToCloudinary.ts
+++ src/cloudinary/uploadImagesToCloudinary.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { v2 as cloudinary } from "cloudinary";
-// import fs from "fs";
+import fs from "fs";
 
 declare global {
   namespace Express {
@@ -20,18 +20,18 @@ const uploadImages = async (
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
   });
-
-
   try {
     const uploadedImageUrls: string[] = [];
 
-    if (req.files && Array.isArray(req.files)) {
-      for (const file of req.files) {
 
-        console.log(file);
-        // const result = await cloudinary.uploader.upload(file.path);
-        // uploadedImageUrls.push(result.secure_url);
-      }
+    console.log(req.body);
+
+    // if (req.files && Array.isArray(req.files)) {
+    //   for (const file of req.files) {
+
+    //     const result = await cloudinary.uploader.upload(file.path);
+    //     uploadedImageUrls.push(result.secure_url);
+    //   }
 
       // if (uploadedImageUrls.length > 0) {
       //   for (const file of req.files) {
@@ -40,9 +40,9 @@ const uploadImages = async (
       // }
       req.uploadedImageUrls = uploadedImageUrls;
       next();
-    } else {
-      res.status(400).json({ error: "No files were uploaded." });
-    }
+    // } else {
+    //   res.status(400).json({ error: "No files were uploaded." });
+    // }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Failed to upload images to Cloudinary" });
